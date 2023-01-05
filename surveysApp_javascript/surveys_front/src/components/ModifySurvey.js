08ebdf9bdbe76c9e88eab53b0d08371cb6ef05a8
liisatormakangas <liisa.tormakangas@gmail.com>
2023-01-05 14:54:55 +0200

    Revert "surveysApp added"
    This reverts commit 168482a78aee4eff9e25303126d5b7a641243b3f.

--- surveysApp_javascript/surveys_front/src/components/ModifySurvey.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState } from "react";
-import { useSurveyContext } from "../contexts/SurveyContext.js";
-
-const ModifySurvey = () => {
-
-    const { modifiedSurvey } = useSurveyContext();
-
-}
